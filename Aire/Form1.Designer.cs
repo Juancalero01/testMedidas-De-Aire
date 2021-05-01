dd008138656cac6aadb1dda421b0aa3228b5b078
Juan <marcelocalero5@gmail.com>
2021-05-01 17:47:11 -0300

    Agregar archivos de proyecto.

---
+++ Aire/Form1.Designer.cs
@@ -0,0 +1,246 @@
+﻿namespace Aire
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.TxtIngresa = new System.Windows.Forms.TextBox();
+            this.TxtSalida1 = new System.Windows.Forms.TextBox();
+            this.TxtSalida2 = new System.Windows.Forms.TextBox();
+            this.TxtSalida3 = new System.Windows.Forms.TextBox();
+            this.TxtSalida4 = new System.Windows.Forms.TextBox();
+            this.ListIngresa = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.BtnResultado = new System.Windows.Forms.Button();
+            this.BtnBorrar = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // TxtIngresa
+            // 
+            this.TxtIngresa.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.TxtIngresa.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.TxtIngresa.Font = new System.Drawing.Font("Microsoft Sans Serif", 25.25F);
+            this.TxtIngresa.Location = new System.Drawing.Point(166, 38);
+            this.TxtIngresa.Name = "TxtIngresa";
+            this.TxtIngresa.Size = new System.Drawing.Size(250, 46);
+            this.TxtIngresa.TabIndex = 0;
+            this.TxtIngresa.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // TxtSalida1
+            // 
+            this.TxtSalida1.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.TxtSalida1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.TxtSalida1.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.25F);
+            this.TxtSalida1.Location = new System.Drawing.Point(166, 205);
+            this.TxtSalida1.Name = "TxtSalida1";
+            this.TxtSalida1.Size = new System.Drawing.Size(250, 41);
+            this.TxtSalida1.TabIndex = 1;
+            this.TxtSalida1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // TxtSalida2
+            // 
+            this.TxtSalida2.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.TxtSalida2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.TxtSalida2.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.25F);
+            this.TxtSalida2.Location = new System.Drawing.Point(166, 278);
+            this.TxtSalida2.Name = "TxtSalida2";
+            this.TxtSalida2.Size = new System.Drawing.Size(250, 41);
+            this.TxtSalida2.TabIndex = 2;
+            this.TxtSalida2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // TxtSalida3
+            // 
+            this.TxtSalida3.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.TxtSalida3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.TxtSalida3.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.25F);
+            this.TxtSalida3.Location = new System.Drawing.Point(166, 349);
+            this.TxtSalida3.Name = "TxtSalida3";
+            this.TxtSalida3.Size = new System.Drawing.Size(250, 41);
+            this.TxtSalida3.TabIndex = 3;
+            this.TxtSalida3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // TxtSalida4
+            // 
+            this.TxtSalida4.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.TxtSalida4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.TxtSalida4.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.25F);
+            this.TxtSalida4.Location = new System.Drawing.Point(166, 420);
+            this.TxtSalida4.Name = "TxtSalida4";
+            this.TxtSalida4.Size = new System.Drawing.Size(250, 41);
+            this.TxtSalida4.TabIndex = 4;
+            this.TxtSalida4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // ListIngresa
+            // 
+            this.ListIngresa.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.ListIngresa.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.ListIngresa.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.ListIngresa.FormattingEnabled = true;
+            this.ListIngresa.ItemHeight = 20;
+            this.ListIngresa.Items.AddRange(new object[] {
+            "FRIGORIAS",
+            "WATTS",
+            "BTU",
+            "TR"});
+            this.ListIngresa.Location = new System.Drawing.Point(247, 90);
+            this.ListIngresa.Name = "ListIngresa";
+            this.ListIngresa.Size = new System.Drawing.Size(169, 82);
+            this.ListIngresa.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.25F);
+            this.label1.Location = new System.Drawing.Point(210, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(166, 26);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "POTENCIA A/A";
+            // 
+            // BtnResultado
+            // 
+            this.BtnResultado.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.BtnResultado.Location = new System.Drawing.Point(166, 90);
+            this.BtnResultado.Name = "BtnResultado";
+            this.BtnResultado.Size = new System.Drawing.Size(75, 37);
+            this.BtnResultado.TabIndex = 7;
+            this.BtnResultado.Text = "Convertir";
+            this.BtnResultado.UseVisualStyleBackColor = false;
+            this.BtnResultado.Click += new System.EventHandler(this.BtnResultado_Click);
+            // 
+            // BtnBorrar
+            // 
+            this.BtnBorrar.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.BtnBorrar.Location = new System.Drawing.Point(166, 133);
+            this.BtnBorrar.Name = "BtnBorrar";
+            this.BtnBorrar.Size = new System.Drawing.Size(75, 37);
+            this.BtnBorrar.TabIndex = 8;
+            this.BtnBorrar.Text = "Borrar";
+            this.BtnBorrar.UseVisualStyleBackColor = false;
+            this.BtnBorrar.Click += new System.EventHandler(this.BtnBorrar_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F);
+            this.label2.Location = new System.Drawing.Point(168, 178);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(110, 24);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "FRIGORIAS";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F);
+            this.label3.Location = new System.Drawing.Point(168, 251);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 24);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "WATTS";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F);
+            this.label4.Location = new System.Drawing.Point(168, 322);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(47, 24);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "BTU";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F);
+            this.label5.Location = new System.Drawing.Point(168, 393);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 24);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "TR";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkGray;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(584, 471);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.BtnBorrar);
+            this.Controls.Add(this.BtnResultado);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ListIngresa);
+            this.Controls.Add(this.TxtSalida4);
+            this.Controls.Add(this.TxtSalida3);
+            this.Controls.Add(this.TxtSalida2);
+            this.Controls.Add(this.TxtSalida1);
+            this.Controls.Add(this.TxtIngresa);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(600, 510);
+            this.MinimumSize = new System.Drawing.Size(600, 510);
+            this.Name = "Form1";
+            this.Opacity = 0.9D;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Conversor A/A v00.01";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox TxtIngresa;
+        private System.Windows.Forms.TextBox TxtSalida1;
+        private System.Windows.Forms.TextBox TxtSalida2;
+        private System.Windows.Forms.TextBox TxtSalida3;
+        private System.Windows.Forms.TextBox TxtSalida4;
+        private System.Windows.Forms.ListBox ListIngresa;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button BtnResultado;
+        private System.Windows.Forms.Button BtnBorrar;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Aire/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Aire
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Resolver()
+        {
+            if (TxtIngresa.Text!="")
+            {
+                double dato = Convert.ToDouble(TxtIngresa.Text);
+                int list = ListIngresa.SelectedIndex;
+                switch (list)
+                {
+                    case 0:
+                        TxtSalida1.Text = dato.ToString();
+                        TxtSalida2.Text = (dato * 1.1628).ToString();
+                        TxtSalida3.Text = (dato * 3.97).ToString();
+                        TxtSalida4.Text = (dato / 3000).ToString();
+                        break;
+                    case 1:
+                        TxtSalida1.Text = (dato * 0.86).ToString();
+                        TxtSalida2.Text = dato.ToString();
+                        TxtSalida3.Text = (dato * 3.412141633).ToString();
+                        TxtSalida4.Text = (dato * 0.000284345).ToString();
+                        break;
+                    case 2:
+                        TxtSalida1.Text = (dato*0.25).ToString();
+                        TxtSalida2.Text = (dato*0.29307107).ToString();
+                        TxtSalida3.Text = dato.ToString();
+                        TxtSalida4.Text = (dato * 0.00008335).ToString();
+                        break;
+                    case 3:
+                        TxtSalida1.Text = (dato * 3000).ToString();
+                        TxtSalida2.Text = (dato * 3516.85).ToString();
+                        TxtSalida3.Text = (dato * 12000).ToString();
+                        TxtSalida4.Text = dato.ToString();
+                        break;
+                }
+            }
+            else
+            {
+                MessageBox.Show("DEBE INGRESAR UN NUMERO");
+            }
+        }
+        private void BtnResultado_Click(object sender, EventArgs e)
+        {
+            Resolver();
+        }
+        private void BtnBorrar_Click(object sender, EventArgs e)
+        {
+            TxtIngresa.Clear();
+            ListIngresa.ClearSelected();
+            TxtSalida1.Clear();
+            TxtSalida2.Clear();
+            TxtSalida3.Clear();
+            TxtSalida4.Clear();
+        }
+    }
+}
